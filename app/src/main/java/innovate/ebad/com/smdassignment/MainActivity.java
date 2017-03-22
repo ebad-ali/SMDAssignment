3c465cbfae95a4d5b77be436d97761d4eb5264f7
ebadali12 <ebad.ali12@yahoo.com>
2017-03-23 04:15:54 +0500

    Intial Change

--- app/src/main/java/innovate/ebad/com/smdassignment/MainActivity.java
+++ app/src/main/java/innovate/ebad/com/smdassignment/MainActivity.java
@@ -1,5 +1,6 @@
 package innovate.ebad.com.smdassignment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -24,21 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
         String name[] = getResources().getStringArray(R.array.names);
 
-        for (int i = 0; i < name.length; i++) {
-            String temp = name[i];
+        String[] numbers = getResources().getStringArray(R.array.numbers);
 
-            String seperate[] = temp.split(" ");
-            seperate[0] = seperate[0].substring(0, 1);
-            seperate[1] = seperate[1].substring(0, 1);
-
-
-            userdata.add(new UserData(name[i], "+92 313 5405894", seperate[0] + seperate[1]));
-        }
+        this.SeperateInitialsAdd(name, numbers);
 
         ArrayAdapter<UserData> adapter = new ContactAdapter(MainActivity.this, userdata);
         listView.setAdapter(adapter);
         listView.setDivider(null);
         listView.setDividerHeight(0);
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -46,10 +41,32 @@ public class MainActivity extends AppCompatActivity {
                                     int position, long id) {
 
                 Toast.makeText(getApplicationContext(),
-                        userdata.get(position).getName(), Toast.LENGTH_LONG)
+                        userdata.get(position).getName(), Toast.LENGTH_SHORT)
                         .show();
+
+                Intent intent = new Intent(MainActivity.this, PersonalActivity.class);
+
+                intent.putExtra("Initial", userdata.get(position).getInitials());
+                intent.putExtra("Name", userdata.get(position).getName());
+                intent.putExtra("PhoneNumber", userdata.get(position).getPhoneNumber());
+                intent.putExtra("Indexcolor", "" + position);
+
+                startActivity(intent);
             }
         });
+    }
+
+    public void SeperateInitialsAdd(String[] name, String[] numbers) {
+        for (int i = 0; i < name.length; i++) {
+            String temp = name[i];
+
+            String seperate[] = temp.split(" ");
+            seperate[0] = seperate[0].substring(0, 1);
+            seperate[1] = seperate[1].substring(0, 1);
+
+            userdata.add(new UserData(name[i], numbers[i], seperate[0] + seperate[1]));
+        }
+
     }
 
 
---
+++ app/src/main/java/innovate/ebad/com/smdassignment/PersonalActivity.java
@@ -0,0 +1,59 @@
+package innovate.ebad.com.smdassignment;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by Ebad on 23/3/2017.
+ */
+
+public class PersonalActivity extends AppCompatActivity {
+
+    TextView intials_view, name_view, phone_view;
+    String[] colorarray;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_contact_layout);
+
+        intials_view = (TextView) findViewById(R.id.initials_field);
+        name_view = (TextView) findViewById(R.id.name_field);
+        phone_view = (TextView) findViewById(R.id.phone_field);
+
+        colorarray = getResources().getStringArray(R.array.colors);
+
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+
+        if (b != null) {
+            String name = (String) b.get("Name");
+            String initial = (String) b.get("Initial");
+            String number = (String) b.get("PhoneNumber");
+            String pos = (String) b.get("Indexcolor");
+            int color_index = Integer.parseInt(pos);
+
+
+            if (color_index >= colorarray.length) {
+                color_index = color_index % colorarray.length;
+                GradientDrawable bgShape = (GradientDrawable) intials_view.getBackground();
+                bgShape.setColor(Color.parseColor(colorarray[color_index]));
+
+            } else {
+                GradientDrawable bgShape = (GradientDrawable) intials_view.getBackground();
+                bgShape.setColor(Color.parseColor(colorarray[color_index]));
+            }
+
+            intials_view.setText(initial);
+            name_view.setText(name);
+            phone_view.setText(number);
+
+        }
+
+    }
+}
