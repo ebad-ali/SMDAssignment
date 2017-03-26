d37e870782be6517876d5eb0d3172bc6de2ff4b2
ebadali12 <ebad.ali12@yahoo.com>
2017-03-26 18:49:05 +0500

    Intial Change

--- app/src/main/java/innovate/ebad/com/contacts/UserData.java
+++ app/src/main/java/innovate/ebad/com/contacts/UserData.java
@@ -1,21 +1,18 @@
 package innovate.ebad.com.contacts;
 
+class UserData {
 
+    private String name;
+    private String phoneNumber;
+    private String initials;
 
-public class UserData {
-
-    String name;
-    String phoneNumber;
-
-    public UserData(String name, String phoneNumber, String initials) {
+    UserData(String name, String phoneNumber, String initials) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.initials = initials;
     }
 
-    String initials;
-
-    public String getInitials() {
+    String getInitials() {
         return initials;
     }
 
